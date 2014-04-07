4c268ab43bc5b25ed39323f72c2ab97a80361c27
ligun <ligun.net@gmail.com>
2014-04-07 17:10:49 +0900

    LEDの点灯パターンを変更

--- sample/Sample03_FullColorLED/Sample03_FullColorLED.ino
+++ sample/Sample03_FullColorLED/Sample03_FullColorLED.ino
@@ -4,17 +4,25 @@
 
 int i =0;
 unsigned char r=0,g=0,b=0;
+unsigned char brightness[256];
 
 void setup(){
   pinMode(RED_PIN,OUTPUT);
   pinMode(GREEN_PIN,OUTPUT);
   pinMode(BLUE_PIN,OUTPUT);
+  
+  for(int i=0;i<128;i++){
+    brightness[i]=i*2;
+  }
+  for(int i=0;i<128;i++){
+    brightness[i+128]=255-i*2;
+  }
 }
 
 void loop(){
-  analogWrite(RED_PIN,r++);
-  if(i%2==0)analogWrite(GREEN_PIN,g++);
-  if(i%3==0)analogWrite(BLUE_PIN,b++);
-  delay(10);
+  analogWrite(RED_PIN,brightness[r++]);
+  if(i%2==0)analogWrite(GREEN_PIN,brightness[g++]);
+  if(i%3==0)analogWrite(BLUE_PIN,brightness[b++]);
+  delay(20);
   i++;
 }
